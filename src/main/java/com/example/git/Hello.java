7972e56ae1452ec6df58e5c2a05b96718d698058
Enoterra <2391037738@qq.com>
2022-06-21 23:31:28 +0800

    first commit

---
+++ src/main/java/com/example/git/Hello.java
@@ -0,0 +1,15 @@
+package com.example.git;
+
+/**
+ * Description
+ *
+ * @author Enoterra
+ * @create 2022-06-21-23:17
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
